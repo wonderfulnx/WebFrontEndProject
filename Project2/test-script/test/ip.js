e3700539cb3753b9a23edac97cda0af598eed05d
wonderfulnx <naxinlegend@outlook.com>
2019-05-29 17:33:10 +0800

    fresh clean

--- Project2/test-script/test/ip.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  var input = ['255.255.255.0', '123.124.123.2', '59.66.137.30'];
-  var expected_output = ['59.66.137.30', '123.124.123.2', '255.255.255.0']
-
-  assert.deepStrictEqual(ip_sort(input), expected_output)
-})()
